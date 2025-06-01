bfd0822e15acc6af3427a82358d9a32775c4460d
dalia-tarek-harira <daliaharira3@gmail.com>
2025-06-01 09:53:46 +0300

    Add files via upload

---
+++ ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using BookSwap.Models;
+
+namespace BookSwap.Interfaces
+{
+    public interface ITokenService
+    {
+        public  Task<string> GenerateAccessToken(ApplicationUser user);
+        public string GenerateRefreshToken();
+
+
+
+    }
+}
